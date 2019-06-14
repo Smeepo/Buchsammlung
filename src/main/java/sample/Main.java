58d349051d0a62d138efbf978d6f7bbabd20a68b
Patrick Laß <plass2@dxc.com>
2019-06-14 09:17:30 +0200

    spring stuff

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -20,6 +20,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(getClass().getResource("/fxml/sample.fxml"));
         rootNode = fxmlLoader.load();
         primaryStage.setTitle("Büchaz");
